d788d1101667933e58af206976b3363f04440569
RAMAZAN DEMİR <rmzn.dmr.info@gmail.com>
2023-08-16 19:05:24 +0300

    page is done

---
+++ kitaplikonline/book-service/book-service/src/main/java/com/kitaplik/bookservice/model/Page.kt
@@ -0,0 +1,18 @@
+package com.kitaplik.bookservice.model
+
+import org.hibernate.annotations.GenericGenerator
+
+@Entity
+@Table(name = "books")
+data class Book @JvmOverloads constructor(
+        @Id
+        @Column(name = "book_id")
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID" , strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String? = "",
+        val title:String,
+        val bookYear:Int,
+        val author:String,
+        val pressName:String,
+        val isbn:String
+)
